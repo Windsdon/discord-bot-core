5af4db4bbefeebfd5f20395e32e1f042ada2eb72
windsdon <Windsdon@users.noreply.github.com>
2016-02-09 09:34:34 -0200

    Updated the "formula"

--- plugins/misc/plugin.js
+++ plugins/misc/plugin.js
@@ -2,7 +2,7 @@ var logger = require("winston");
 var CryptoJS = require("crypto-js");
 
 module.exports = {
-    version: "1.0.0",
+    version: "1.0.1",
     name: "Misc",
     author: "Windsdon",
     init: MiscMod
@@ -23,8 +23,12 @@ function MiscMod(e, callback) {
 function size(e, args) {
     var subject = args.user || e.userID;
     var hash = CryptoJS.MD5(subject).toString();
-    var n = parseInt(hash.substring(5, 9), 16);
-    var r = n/32768 - 1;
-    var s = Math.abs(15 + r * 5);
-    e.respond(e.getName(subject) + " is " + s.toFixed(1) + "cm.");
+    var n = (parseInt(hash.substring(5, 9), 16) * 4) % 0xffff;
+    var u = n/32768 - 1; // [-1,1]
+    if(u == 0) {
+        u = 0.001;
+    }
+    var r = Math.pow(0.5 - Math.cos(Math.PI * u)/2, 1.5) * (u/Math.abs(u));
+    var s = Math.abs(15 + r * 10);
+    e.respond(e.getName(subject) + " is **" + s.toFixed(1) + "cm**");
 }
